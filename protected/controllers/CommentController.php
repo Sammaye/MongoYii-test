b40a0b2304aa0f6b40d7247259ea0acd8048cb40
Sammaye <sam.millman@googlemail.com>
2013-06-01 19:50:49 +0100

    Added more doc comments and cleaned up more

--- protected/controllers/CommentController.php
+++ protected/controllers/CommentController.php
@@ -34,45 +34,51 @@ class CommentController extends CController{
 		$this->render('index');
 	}
 	
+	/**
+	 * This is the action that creates a new comment on a post. The article view page will route an 
+	 * ajax call to here. This function will only work from Ajax and no other source, also it will only 
+	 * accept a post request. Upon successfully adding a new comment it will return the new comments HTML. 
+	 * If it is unsuccessful it will return the model errors.
+	 * @throws CHttpException
+	 */
 	public function actionCreate(){
 		
-		if(Yii::app()->request->getIsPostRequest()){
-			
-			if(isset($_POST['Comment'])){
+		if(Yii::app()->request->getIsPostRequest()||!Yii::app()->request->getIsAjaxRequest()){
 
+			if(isset($_POST['Comment'])){
 				$model=new Comment();
 				$model->attributes=$_POST['Comment'];
 				if($model->validate()&&$model->save()){		
-					if(Yii::app()->request->getIsAjaxRequest()){
 						
-						// return html render
-						ob_start();
-						$this->renderPartial('_comment',array('model'=>$model));
-						$html=ob_get_contents();
-						ob_clean();
+					// return html render
+					ob_start();
+					$this->renderPartial('_comment',array('model'=>$model));
+					$html=ob_get_contents();
+					ob_clean();
 						
-						echo json_encode(array('success'=>true,'html'=>$html));
-						
-					}else{
-						// redirect
-						$this->redirect(array('article/view','id'=>$model->articleId));
-					}
+					echo json_encode(array('success'=>true,'html'=>$html));
+					Yii::app()->end();
 				}else{
-					if(Yii::app()->request->getIsAjaxRequest()){
-						echo json_encode(array('success'=>false,'errors'=>$model->getErrors()));
-					}else{
-						// Do something fancy with error handling here
-					}
+					echo json_encode(array('success'=>false,'errors'=>$model->getErrors()));
+					Yii::app()->end();
 				}
-			}
+			}else 
+				echo json_encode(array('success'=>true,'html'=>''));
 			
 		}else 
 			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
-		
 	}
 	
+	/**
+	 * This is the action which deletes a comment. It has all the same properties of the action above 
+	 * to create a comment except it will only allow a deletion from either an admin or from the owner. 
+	 * I could make this into a rule and say that all rules should allow admin access but for now it is 
+	 * hard coded like this.
+	 * @param string $id
+	 * @throws CHttpException
+	 */
 	public function actionDelete($id){
-		if(Yii::app()->request->getIsPostRequest()){
+		if(Yii::app()->request->getIsPostRequest()||!Yii::app()->request->getIsAjaxRequest()){
 			
 			$model=Comment::model()->findBy_id($id);
 			if(
