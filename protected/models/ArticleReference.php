a42c42af88d8c1286f94bdc35cd350b69d247b0c
Sammaye <sam.millman@googlemail.com>
2013-06-02 14:00:46 +0100

    Started adding article reference for completion of subdocument examples

--- protected/models/ArticleReference.php
+++ protected/models/ArticleReference.php
@@ -1,5 +1,25 @@
 <?php
+
+/**
+ * This model represents an article reference. These are basically URLs for support the facts within the article.
+ * 
+ * This will be stored as a subdocument on the article record.
+ */
 class ArticleReference extends EMongoModel{
 
+	public $caption;
+	public $url;
 	
+	public function rules(){
+		return array(
+			array('caption,url','required'),
+			array('caption','length','max'=>255),
+			array('url','length','max'=>2000),
+			array('url','url')		
+		);
+	}
+	
+	public function renderUrl(){
+		echo CHtml::link($caption,$url);
+	}
 }
