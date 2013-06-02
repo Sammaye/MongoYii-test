25e8ee0c021bf408e1ac9e4f0c81c496f918d5c9
Sammaye <sam.millman@googlemail.com>
2013-06-02 09:47:30 +0100

    Cleaned up the site some more

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -36,7 +36,6 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'View Articles', 'url'=>array('/article')),
 
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,15 +6,21 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<p>Congratulations! You have successfully ran the MongoYii tester application.</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
+<p>This applications code is designed to give you an overview on how to use MongoYii to accomplish your goals with MongoDB using the Yii framework.</p>
+
+<p>The premise of this application is to provide user session and role management in a wikipedia type format.</p>
+
+<p>Some of the main areas that will be covered:</p>
 <ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+	<li>Models</li>
+	<li>controllers</li>
+	<li>Relations, both referencing and embedded</li>
+	<li>Widgets</li>
 </ul>
 
 <p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+the <a href="http://sammaye.github.io/MongoYii/">documentation</a>.
+Feel free to ask either on <a href="http://www.stackoverflow.com">stackoverflow</a> (which I frequent) 
+or raise a <a href="https://github.com/Sammaye/MongoYii/issues?sort=created&state=open">new issue on Github</a>.</p>
